f575d66677e72e298d0ce4b57a3b0b850635b82e
dirien <engin.diri@mail.schwarz>
2022-10-08 13:54:25 +0000

    rust-jreleaser v0.1.0

--- Formula/rust-jreleaser.rb
+++ Formula/rust-jreleaser.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.2.0 at 2022-10-08T15:25:13.165972+02:00
+# Generated with JReleaser 1.2.0 at 2022-10-08T13:54:17.311799615Z
 class RustJreleaser < Formula
   desc "A very, very simple Hello World application written in Rust"
   homepage ""
   url "https://github.com/dirien/rust-jreleaser/releases/download/v0.1.0/rust-jreleaser-0.1.0-darwin-amd64.zip"
   version "0.1.0"
-  sha256 "b6ab4504025255b7dd2ae9d0e72ec113f91a83a49a0e898e77c559fd9f5a7864"
+  sha256 "a29e543c276a41c85c57285a8c21dacfef84fb0ff2dbd74cf18b09c3d8f0222e"
   license "Apache-2.0"
 
 
